edb48a8775d449e08ce7e3b2bfa21cd1937e9711
Gerasimovich Sergey <wmmoee@gmail.com>
2025-03-25 02:17:56 +0300

    add notifications with react-toastify

--- src/pages/ToDoListPage.tsx
+++ src/pages/ToDoListPage.tsx
@@ -3,6 +3,7 @@ import { Form } from '../components/Form/Form';
 import { ToDoList } from '../components/ToDoList/ToDoList';
 import { useState } from 'react';
 import { ToDo } from '../models/todo-item';
+import { Flip, toast, ToastContainer } from 'react-toastify';
 
 
 export const ToDoListPage = () => {
@@ -16,12 +17,24 @@ export const ToDoListPage = () => {
         };
 
         setTodos([...todos, newTodo]);
+
+        const notify = () => {
+            return toast(`Добавил новую задачу: "${text}"`);
+        };
+
+        notify();
     };
 
     const updateTodo = (todoItem: ToDo) => {
         const newTodos = todos.map((todo) => {
             if (todo.id === todoItem.id) {
                 todo.isDone = !todo.isDone;
+
+                const notify = () => {
+                    return toast(`Изменил статус задачи: "${todoItem.text}" на "${todo.isDone ? 'Выполнена' : 'Не выполнена'}"`);
+                }
+
+                notify();
             }
 
             return todo;
@@ -32,6 +45,14 @@ export const ToDoListPage = () => {
 
     const deleteTodo = (todoItem: ToDo) => {
         const newTodos = todos.filter((todo) => {
+            if (todoItem.id === todo.id) {
+                const notify = () => {
+                    return toast(`Удалил задачу: "${todoItem.text}"`);
+                }
+
+                notify();
+            }
+
             return todo.id !== todoItem.id;
         });
 
@@ -43,6 +64,19 @@ export const ToDoListPage = () => {
             <Header />
             <Form createNewTodo={createNewTodo} />
             <ToDoList todos={todos} updateTodo={updateTodo} deleteTodo={deleteTodo} />
+            <ToastContainer
+                position="bottom-right"
+                autoClose={8000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+                transition={Flip}
+            />
         </>
     );
 };
